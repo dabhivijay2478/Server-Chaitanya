6ad0cfe69eac0545973736a14a99118f3940f554
vijaydabhi <dabhivijay2478@gmail.com>
2023-07-20 20:24:49 +0530

    update index.js

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 app.use(express.json());
 app.use('/', require('./Router/routes'));
 
